11b8fbc4f16a20ff83580126a6bd70bdd4af9791
ajorakun <106663193+ajorakun@users.noreply.github.com>
2022-06-01 04:47:43 -0700

    Create plu.sh

---
+++ plu.sh
@@ -0,0 +1,9 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y &&
+sudo apt install git -y &&
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git 
+cd ccminer &&
+chmod +x build.sh &&
+chmod +x configure.sh &&
+chmod +x autogen.sh &&
+./build.sh &&
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956#xnsub -u RQnT3LHSZGdaLSoWuer87rLiDC99Rnh2sP.Circle -p x -x socks5://192.252.211.197:14921
